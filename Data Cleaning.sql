90a86513d1942cb77f2eb03c4808b51deae64b13
Alpesh sorte <140254402+Alpeshsorte@users.noreply.github.com>
2025-05-07 14:07:29 +0530

    Add files via upload

---
+++ Data Cleaning.sql
@@ -0,0 +1,125 @@
+-- Data ceaning progect
+select *
+from layoffs;
+
+-- --1.remove duplicates
+-- --2.standardize data
+-- --3.null value and blank value
+-- --4. remove any columns
+
+create table layoffs_staging
+like layoffs;
+select *
+from layoffs_staging;
+
+insert layoffs_staging
+select *
+from layoffs;
+
+select *,
+row_number() over(
+partition by company, industry, total_laid_off, percentage_laid_off, 'date') as row_num
+from layoffs_staging;
+
+with duplicate_cte as
+(
+select *,
+row_number() over(
+partition by company, location,
+industry, total_laid_off, percentage_laid_off, stage, country, funds_raised_millions, 'date') as row_num
+from layoffs_staging
+)
+select *
+from duplicate_cte
+where row_num > 1;
+
+select *
+from layoffs_staging
+where company='casper';
+
+delete 
+from duplicate_cte
+where row_num > 1;
+
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select *
+from layoffs_staging2
+where row_num > 1;
+
+insert into layoffs_staging2
+select *,
+row_number() over(
+partition by company, location,
+industry, total_laid_off, percentage_laid_off, stage,
+country, funds_raised_millions, 'date') as row_num
+from layoffs_staging;
+
+delete
+from layoffs_staging2
+where row_num > 1;
+
+select *
+from layoffs_staging2;
+
+-- standardizing data
+
+
+update layoffs_staging2
+set company=trim(company);
+
+select distinct industry
+from layoffs_staging2;
+
+
+update layoffs_staging2
+set industry = 'crypto'
+where industry like 'crypto%';
+
+select *
+from layoffs_staging2
+where country like 'united states%'
+order by 1;
+
+select `date`,
+str_to_date(`date`, '%m/%d/%y' )
+from layoffs_staging2;
+
+select `date`
+from layoffs_staging2;
+
+update layoffs_staging2
+set `date` = str_to_date(`date`, '%m/%d/%Y');
+
+alter table layoffs_staging2
+modify column `date` DATE;
+
+select *
+from layoffs_staging2
+where industry is null
+or industry = '';
+
+select *
+from layoffs_staging2
+where company='airbnb';
+
+select t1.location=t2.location
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+on t1.company=t2.company
+and t1.location=t2.location
+where (t1.industry is null or t1.industry='')
+and t1.industry is not null
+
